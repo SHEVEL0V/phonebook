47e08acfb35b774e239753ce2164263305c76d82
Андрій Шевельов <ashevelevmti@gmail.com>
2022-08-16 13:45:51 +0300

    add

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,8 +20,3 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
